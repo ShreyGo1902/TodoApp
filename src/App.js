e4038a3cc96efcdf470776bf1d4b6f21d7167323
Your Name <you@example.com>
2019-11-09 19:32:17 +0530

    Deleted ReadMe

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ToDos from './components/ToDos'
+import Header from './components/layout/Header'
+import AddToDo from './components/AddTodo'
+import uuid from 'uuid'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import About from './components/pages/about'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+class ToDo extends Component {
+
+  state = {
+    todos: [] //array of objects
+  }
+  markComplete = (id) => {
+    this.setState({todos : this.state.todos.map(todo => {
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo;
+    })})    
+  }
+
+  delTodo = (id) => {
+    this.setState({todos : [...this.state.todos.filter(todo => todo.id !== id)]})
+  }
+
+  addTodo = (title) => {
+    const newTodo = {
+      id:uuid.v4(),
+      title : title,
+      completed : false
+    }
+    this.setState({ todos : [...this.state.todos, newTodo]})
+  }
+  render() { 
+    return (
+      <Router>
+        <React.Fragment>
+          <div className = "container">
+
+            <Header />
+            <Route exact path = "/" render = {props => (
+              <React.Fragment>
+                <AddToDo addTodo = {this.addTodo}/>
+                <ToDos todos = {this.state.todos} markComplete = {this.markComplete} delTodo = {this.delTodo}/>
+              </React.Fragment>
+            )}/>  
+
+            <Route path = "/about" component = {About}
+            />
+
+          </div>
+        </React.Fragment>
+      </Router>
+    );
+  }
 }
-
-export default App;
+ 
+export default ToDo;
